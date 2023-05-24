d61aa5e575928945fda11029f610315e2c9ba082
Mauricio-tech <mauricio.trevinon91@gmail.com>
2023-05-23 22:30:44 -0600

    fixed connection.js

--- config/conection.js
+++ config/conection.js
@@ -4,7 +4,14 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/userThoughtDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
+});
+
+// Handling connection events
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 // Exporting the connection to be used elsewhere in the app
-module.exports = mongoose.connection;
+module.exports = db;
